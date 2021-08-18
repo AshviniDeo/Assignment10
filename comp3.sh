a91043d97805672556e4f9f9a52abf38e16303a9
ashvini deokate <ashvinideokate3@gmail.com>
2021-08-19 04:44:47 +0530

    Computation 3

---
+++ comp3.sh
@@ -0,0 +1,7 @@
+read -p "Enter number a : " a
+read -p "Enter number b : " b
+read -p "Enter number c : " c
+
+compute3=$(($c+$a/$b))
+echo "Computation for c+a/b with value $a , $b and $c is : $compute3"
+
