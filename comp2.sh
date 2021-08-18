90846650721146dc8b1a2be50da25ca7c08e4b61
ashvini deokate <ashvinideokate3@gmail.com>
2021-08-19 04:40:34 +0530

    Computation 2

---
+++ comp2.sh
@@ -0,0 +1,8 @@
+read -p "Enter number a : " a
+read -p "Enter number b : " b
+read -p "Enter number c : " c
+
+compute2=$(($a*$b+$c))
+echo "Computation for a*b+c with value $a , $b and $c is : $compute2"
+
+
